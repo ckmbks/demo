0bad12a43e8d0f78f67b019298cda4994f060bbd
ckmbks <522477057@qq.com>
2019-03-27 15:47:48 +0800

    2019年3月27日

---
+++ demo.web/src/components/Table/BasePage.vue
@@ -0,0 +1,41 @@
+<script>
+  export default {
+    name: 'BasePage',
+    filters: {},
+    data() {
+      return {
+        list: null,
+        total: 0,
+        listLoading: true,
+        listQuery: {
+          page: 1,
+          limit: 20
+        }
+      }
+    },
+    methods: {
+      refresh() {
+        this.listLoading = true
+        this.getPage(this.listQuery).then(response => {
+          this.list = response.data.list
+          this.total = response.data.total
+          this.listLoading = false
+        })
+      },
+      getPage() {
+      },
+      search() {
+        this.listQuery.page = 1
+        this.refresh()
+      },
+      handleSizeChange(val) {
+        this.listQuery.limit = val
+        this.refresh()
+      },
+      handleCurrentChange(val) {
+        this.listQuery.page = val
+        this.refresh()
+      }
+    }
+  }
+</script>
---
+++ demo.web/src/components/Table/PageTable.vue
@@ -0,0 +1,37 @@
+<template xmlns:slot="http://www.w3.org/1999/html">
+  <div class="app-container">
+    <slot name="search"></slot>
+
+    <el-table v-loading="listLoading" :data="list" height="600" border fit highlight-current-row style="width: 100%">
+      <slot name="table"></slot>
+    </el-table>
+    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit"
+                @pagination="search"/>
+  </div>
+</template>
+
+<script>
+  import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+
+  export default {
+    name: 'PageTable',
+    components: {Pagination},
+    filters: {},
+    methods: {
+      search() {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .edit-input {
+    padding-right: 100px;
+  }
+
+  .cancel-btn {
+    position: absolute;
+    right: 15px;
+    top: 10px;
+  }
+</style>
